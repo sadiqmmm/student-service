08c12394a700dca233302badc55431982eef6c85
jordanhudgens <jordan@devcamp.com>
2018-08-24 13:12:22 -0700

    Setup default values needed for managing the sessions

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,4 @@
+# TODO
+# Set custom domain
+# https://stackoverflow.com/questions/4060333/what-does-rails-3-session-store-domain-all-really-do
+Rails.application.config.session_store :cookie_store, key: '_devcamp_space_session', domain: 'devcamp.space'
