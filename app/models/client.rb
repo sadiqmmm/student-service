0fb19bd0af744c22a49c001559786971300bb0b1
jordanhudgens <jordan@devcamp.com>
2018-08-24 14:37:05 -0700

    Connected relationships between the client and portfolio items

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,4 +2,5 @@ class Client < ApplicationRecord
   has_secure_password
 
   has_many :portfolio_app_users, dependent: :destroy
+  has_many :portfolio_items, dependent: :destroy
 end
--- app/models/portfolio_app_user.rb
+++ app/models/portfolio_app_user.rb
@@ -1,4 +1,6 @@
 class PortfolioAppUser < ApplicationRecord
+  has_secure_password
+
   belongs_to :client
   belongs_to :app_template
 end
