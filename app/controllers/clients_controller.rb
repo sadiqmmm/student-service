3041bac3b63199e18bc1a4bcbdb9560f30c517a6
jordanhudgens <jordan@devcamp.com>
2018-08-24 14:14:57 -0700

    Cleaned up routes and clients controller

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,25 +1,20 @@
 class ClientsController < ApplicationController
   before_action :set_client, only: [:show, :edit, :update, :destroy]
 
-  # GET /clients
   def index
     @clients = Client.all
   end
 
-  # GET /clients/1
   def show
   end
 
-  # GET /clients/new
   def new
     @client = Client.new
   end
 
-  # GET /clients/1/edit
   def edit
   end
 
-  # POST /clients
   def create
     @client = Client.new(client_params)
 
@@ -30,7 +25,6 @@ class ClientsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /clients/1
   def update
     if @client.update(client_params)
       redirect_to @client, notice: 'Client was successfully updated.'
@@ -39,19 +33,16 @@ class ClientsController < ApplicationController
     end
   end
 
-  # DELETE /clients/1
   def destroy
     @client.destroy
     redirect_to clients_url, notice: 'Client was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_client
       @client = Client.find(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
     def client_params
       params.require(:client).permit(:email, :password_digest)
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   resources :app_templates
   resources :clients
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
