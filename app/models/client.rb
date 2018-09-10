fcf9bde3f440e097426f25426249481d25763332
jordanhudgens <jordan@devcamp.com>
2018-09-10 13:43:15 -0700

    Added a validation for the password and email

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,7 +1,8 @@
 class Client < ApplicationRecord
   has_secure_password
 
-  validates :subdomain, format: { with: /\A[a-zA-Z0-9]+\Z/ }
+  validates :subdomain, format: { with: /\A[a-zA-Z0-9]+\Z/ }, presence: true
+  validates_presence_of :email, :password_digest
 
   has_many :portfolio_app_users, dependent: :destroy
   has_many :portfolio_items, dependent: :destroy
