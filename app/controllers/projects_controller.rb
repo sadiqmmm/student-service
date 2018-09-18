5b9d453cc76fc2164093999c2e9823296d846c05
jordanhudgens <jordan@devcamp.com>
2018-09-18 10:22:17 -0500

    Added debugging code to test for current user

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,10 @@
 class ProjectsController < ApplicationController
+  include AuthenticationConcern
+
   def index
     @projects = Project.all
+
+    puts "CURRENT CLIENT" * 100, @current_client.inspect, "CURRENT CLIENT" * 100
 
     render json: @projects
   end
