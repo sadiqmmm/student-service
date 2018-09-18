9a6501efed7a99b7e63e8b14c26c1b725ce7b9cd
jordanhudgens <jordan@devcamp.com>
2018-09-18 10:02:14 -0500

    Added the sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   resources :portfolio_app_users
   resources :app_templates
   resources :clients
+  resources :sessions, only: [:create]
+  delete :logout, to: 'sessions#logout'
+  get :logged_in, to: 'sessions#logged_in'
   get 'get-client-applications', to: 'clients#get_client_applications'
-
   root to: 'home#index'
 end
