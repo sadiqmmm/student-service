ebf0b831340499d997e42c584c4f6c674da6b175
jordanhudgens <jordan@devcamp.com>
2018-09-18 10:04:18 -0500

    Created the session controller with a login, check login, and logout set of actions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,29 @@
+class SessionsController < ApplicationController
+  include AuthenticationConcern
+
+  def create
+    client = Client
+            .find_by(email: params['user']['email'])
+            .try(:authenticate, params['user']['password'])
+
+    if client
+      session[:client_id] = client.id
+      render json: { status: :created }
+    else
+      render json: { status: 500 }
+    end
+  end
+
+  def logged_in
+    if @current_client
+      render json: { logged_in: true }
+    else
+      render json: { logged_in: false }
+    end
+  end
+
+  def logout
+    reset_session
+    render json: { status: 200 }
+  end
+end
