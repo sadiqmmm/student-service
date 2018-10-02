5fe91ee83dcbe48b90c72950e35ecc340fdbef54
jordanhudgens <jordan@devcamp.com>
2018-10-02 15:56:21 -0500

    Added slug attr to the projects table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_02_204559) do
+ActiveRecord::Schema.define(version: 2018_10_02_205544) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 2018_10_02_204559) do
     t.datetime "updated_at", null: false
     t.string "language"
     t.string "logo"
+    t.text "slug"
   end
 
   add_foreign_key "portfolio_app_users", "app_templates"
