db425076d3f38ccfdf73fd29989a2e39c6258e53
jordanhudgens <jordan@devcamp.com>
2018-10-27 14:22:03 -0500

    Created virtual attrs for the images and called them from the portfolio item serializer

--- app/models/portfolio_item.rb
+++ app/models/portfolio_item.rb
@@ -2,4 +2,16 @@ class PortfolioItem < ApplicationRecord
   belongs_to :client
   has_one_attached :thumb_image
   has_one_attached :banner_image
+
+  def thumb_image_url
+    if self.thumb_image.attachment
+      self.thumb_image.attachment.service_url
+    end
+  end
+
+  def banner_image_url
+    if self.banner_image.attachment
+      self.banner_image.attachment.service_url
+    end
+  end
 end
--- app/serializers/portfolio_item_serializer.rb
+++ app/serializers/portfolio_item_serializer.rb
@@ -1,4 +1,10 @@
 class PortfolioItemSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :url
+  attributes :id,
+             :name,
+             :description,
+             :url,
+             :thumb_image_url,
+             :banner_image_url
+
   has_one :client
 end
