a3412fe9f94c8cb651b41579dbd39a0600015664
jordanhudgens <jordan@devcamp.com>
2018-10-29 16:42:36 -0500

    Moved the cors config back into the initializer

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,17 +1,17 @@
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'https://www.devcamp.space'
-#     resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
-#   end
-#
-#   allow do
-#     origins ['http://localhost:3000', 'http://localhost:8000']
-#     resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
-#   end
-#
-#   allow do
-#     origins ClientDomain.all.pluck(:url)
-#     puts "client domains" * 500, ClientDomain.all.pluck(:url), "client domains" * 500
-#     resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'https://www.devcamp.space'
+    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+  end
+
+  allow do
+    origins ['http://localhost:3000', 'http://localhost:8000']
+    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+  end
+
+  allow do
+    origins ClientDomain.all.pluck(:url)
+    puts "client domains" * 500, ClientDomain.all.pluck(:url), "client domains" * 500
+    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+  end
+end
