422a39d5fdbe20e2392fec51bb77f53fef591896
jordanhudgens <jordan@devcamp.com>
2018-10-29 14:07:22 -0500

    Migrated the db to include the new client domain resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_29_190441) do
+ActiveRecord::Schema.define(version: 2018_10_29_190629) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,14 @@ ActiveRecord::Schema.define(version: 2018_10_29_190441) do
     t.integer "status", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "client_domains", force: :cascade do |t|
+    t.string "url"
+    t.bigint "client_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["client_id"], name: "index_client_domains_on_client_id"
   end
 
   create_table "clients", force: :cascade do |t|
@@ -105,6 +113,7 @@ ActiveRecord::Schema.define(version: 2018_10_29_190441) do
     t.text "white_logo"
   end
 
+  add_foreign_key "client_domains", "clients"
   add_foreign_key "endpoints", "projects"
   add_foreign_key "portfolio_app_users", "app_templates"
   add_foreign_key "portfolio_app_users", "clients"
