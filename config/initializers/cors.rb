48f9f5ef5033cad53e4d255f65e2f32c0ed97339
jordanhudgens <jordan@devcamp.com>
2018-10-29 13:26:35 -0500

    Passed an array to the cors loader

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
-    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    origins 'https://www.devcamp.space'
+    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+  end
+
+  allow do
+    origins ['http://localhost:3000', 'http://localhost:8000']
+    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 end
