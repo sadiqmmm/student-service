856399b6c29d3f8c0909218eb7ea627f281bc35a
jordanhudgens <jordan@devcamp.com>
2018-10-30 10:10:35 -0500

    Temp removed other origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,13 +1,13 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins 'https://www.devcamp.space'
-    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
-  end
-
-  allow do
-    origins ['http://localhost:3000', 'http://localhost:8000']
-    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
-  end
+  # allow do
+  #   origins 'https://www.devcamp.space'
+  #   resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+  # end
+  #
+  # allow do
+  #   origins ['http://localhost:3000', 'http://localhost:8000']
+  #   resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+  # end
 
   allow do
     origins do |origin|
