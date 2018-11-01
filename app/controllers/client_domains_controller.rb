f53fcb540d95b98a9151be506f4d2f4450a37fd3
jordanhudgens <jordan@devcamp.com>
2018-11-01 16:34:47 -0500

    Created a create action for the white list client domain resource

--- app/controllers/client_domains_controller.rb
+++ app/controllers/client_domains_controller.rb
@@ -5,4 +5,22 @@ class ClientDomainsController < ApplicationController
     @client_domains = @current_client.client_domains
     render json: @client_domains
   end
+
+  def create
+    @client_domain = @current_client.client_domains.new(client_domain_params)
+
+    if @client_domain.save
+      render json: @client_domain, status: :created
+    else
+      render json: @client_domain.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+    def client_domain_params
+      params.require(:client_domain).permit(
+        :url,
+      )
+    end
 end
