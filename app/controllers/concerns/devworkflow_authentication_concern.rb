92f3d219d7fa771e017d2f998a44f3a12ba51401
jordanhudgens <jordan@devcamp.com>
2018-11-10 11:51:21 -0700

    Updated concern for setting the devworkflow user

--- app/controllers/concerns/devworkflow_authentication_concern.rb
+++ app/controllers/concerns/devworkflow_authentication_concern.rb
@@ -5,7 +5,7 @@ module DevworkflowAuthenticationConcern
     before_action :set_devworkflow_current_user
   end
 
-  def set_current_user
+  def set_devworkflow_current_user
     if session[:devworkflow_user_id]
       @devworkflow_current_user = DevworkflowUser.find(session[:devworkflow_user_id])
     end
