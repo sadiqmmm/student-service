3c14ff8726554f057b151fe914c3f28bd0018ed0
jordanhudgens <jordan@devcamp.com>
2018-11-14 12:24:58 -0700

    Added more items to the filter for devworkflow users

--- app/models/devworkflow_user.rb
+++ app/models/devworkflow_user.rb
@@ -10,7 +10,7 @@ class DevworkflowUser < ApplicationRecord
 
   def column_names_merged_with_images
     DevworkflowUser.column_names
-      .select { |column| !['client_id', 'password_digest'].include?(column) }
+      .select { |column| !['client_id', 'password_digest', 'created_at', 'updated_at'].include?(column) }
       .push(['password', 'password_confirmation'])
       .flatten
       .uniq
