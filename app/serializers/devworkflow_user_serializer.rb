e22954a5bff8e5c189c9e4d7b85242d61e82a72c
jordanhudgens <jordan@devcamp.com>
2018-11-26 17:20:20 -0600

    Updated the value returned from the serializer

--- app/serializers/devworkflow_user_serializer.rb
+++ app/serializers/devworkflow_user_serializer.rb
@@ -2,5 +2,7 @@ class DevworkflowUserSerializer < ActiveModel::Serializer
   attributes :id,
              :name,
              :email,
+             :password,
+             :password,
              :column_names_merged_with_images
 end
