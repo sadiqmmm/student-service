26490ed5257289c1e818def3f7680639fc906dfc
jordanhudgens <jordan@devcamp.com>
2018-11-26 17:56:59 -0600

    Added the devworkflow user id to the serializer

--- app/serializers/devworkflow_project_serializer.rb
+++ app/serializers/devworkflow_project_serializer.rb
@@ -2,5 +2,6 @@ class DevworkflowProjectSerializer < ActiveModel::Serializer
   attributes :id,
              :title,
              :main_objective,
+             :devworkflow_user_id,
              :column_names_merged_with_images
 end
