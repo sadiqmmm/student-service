9415ea57955f5cfc7b2be0603b15b91853f66d48
jordanhudgens <jordan@devcamp.com>
2018-11-26 17:52:34 -0600

    Removed the objects from the devworkflow serializer

--- app/serializers/devworkflow_project_serializer.rb
+++ app/serializers/devworkflow_project_serializer.rb
@@ -3,7 +3,4 @@ class DevworkflowProjectSerializer < ActiveModel::Serializer
              :title,
              :main_objective,
              :column_names_merged_with_images
-
-  belongs_to :devworkflow_user
-  has_many :devworkflow_project_line_items
 end
