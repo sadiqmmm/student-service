89dbcef9259cfa3cf040ff7efeb8b70bfa7fdde4
jordanhudgens <jordan@devcamp.com>
2019-01-16 15:53:12 -0600

    Added a conditional for the removal of portfolio images

--- app/controllers/portfolio/portfolio_item_images_controller.rb
+++ app/controllers/portfolio/portfolio_item_images_controller.rb
@@ -3,8 +3,18 @@ class Portfolio::PortfolioItemImagesController < ApplicationController
   include AuthenticationConcern
 
   def destroy
+    list_of_images = ["thumb_image", "banner_image", "logo"]
+
     if @portfolio_item.client == @current_client
-      puts "DESTROY" * 500, @portfolio_item.inspect, "destroy" * 500, params.inspect
+      if params["image_type"] && list_of_images.include?(params["image_type"])
+        puts "PORTFOLIO_ITEM" * 500, @portfolio_item.thumb_image.service_url, "portfolio_item" * 500
+        @portfolio_item.send(params["image_type"]).purge
+        puts "PORTFOLIO_ITEM" * 500, @portfolio_item.thumb_image.service_url, "portfolio_item" * 500
+        puts "params" * 500, params.inspect, "PARAMS" * 500
+        render json: { status: 204 }
+      else
+        render json: { status: :unprocessable_entity }
+      end
     else
       render json: { status: :unauthorized }
     end
