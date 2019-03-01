d396b7b8a530cfc64ae49bf4c94ba78c2c63a314
jordanhudgens <jordan@devcamp.com>
2019-03-01 15:22:23 -0600

    Updated cors policy to include additional localhost ports

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,7 +10,11 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins ['http://localhost:3000', 'http://localhost:8000']
+    origins ['http://localhost:3000',
+             'http://localhost:3002',
+             'http://localhost:3008',
+             'http://localhost:5000',
+             'http://localhost:8000']
     resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 
