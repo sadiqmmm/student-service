152ba8f0d02d5b7147bbd9ed4204804db1f3f19b
jordanhudgens <jordan@devcamp.com>
2019-03-01 15:23:07 -0600

    Added a plain localhost call in cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,7 +10,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins ['http://localhost:3000',
+    origins ['http://localhost',
+             'http://localhost:3002',
              'http://localhost:3002',
              'http://localhost:3008',
              'http://localhost:5000',
