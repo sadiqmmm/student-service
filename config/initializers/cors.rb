f62d96e6953e77a4be720e1fea5ba824a1e782e9
jordanhudgens <jordan@devcamp.com>
2019-09-13 14:30:36 -0700

    Updated cors configuration

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,11 +10,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins ['http://localhost:3000',
-             'http://localhost:3002',
-             'http://localhost:3008',
-             'http://localhost:5000',
-             'http://localhost:8080']
+    origins(/\Ahttps?:\/\/([a-zA-Z\d-]+\.){0,}localhost:\d*\/?\z/)
     resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 
