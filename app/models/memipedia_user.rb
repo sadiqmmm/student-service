3b84605456a6390e09036cac429c6d646e1fc046
jordanhudgens <jordan@devcamp.com>
2020-03-05 13:47:35 -0700

    Added validations and has secure password flag

--- app/models/memipedia_user.rb
+++ app/models/memipedia_user.rb
@@ -1,4 +1,10 @@
 class MemipediaUser < ApplicationRecord
+  has_secure_password
+
+  # TODO
+  # scope validation through client
+  validates_presence_of :email
+
   belongs_to :client
   belongs_to :app_template
 end
