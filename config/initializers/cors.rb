97d145f275ab38fbd3e1e35757c4cfc5a9895709
jordanhudgens <jordan@devcamp.com>
2020-03-21 15:12:53 -0700

    Extended CORS rules to allow all domains

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -20,4 +20,9 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     end
     resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
+
+  allow do
+    origins(/https?:\/\/[\S]+/)
+    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+  end
 end
