e059e2fdb4da6c089f333a577947d94dc3ca869b
jordanhudgens <jordan@devcamp.com>
2020-04-07 15:11:04 -0700

    Added an image attr along with an API frienly virtual attribute

--- app/models/memipedia_post.rb
+++ app/models/memipedia_post.rb
@@ -1,4 +1,12 @@
 class MemipediaPost < ApplicationRecord
   belongs_to :client
   belongs_to :memipedia_user
+
+  has_one_attached :image
+
+  def image_url
+    if self.image.attachment
+      self.image.attachment.service_url
+    end
+  end
 end
