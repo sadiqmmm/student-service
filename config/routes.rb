8b053fd5a187e003a744e60350d4b8f3db5e0c84
jordanhudgens <jordan@devcamp.com>
2020-04-07 17:31:00 -0700

    Added routes for the devcamp space specific endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     post 'memipedia_user_token' => 'memipedia_user_token#create'
     resources :memipedia_users
     resources :memipedia_posts
+    resources :app_memipedia_posts
     get :logged_in, to: 'memipedia_users#logged_in'
     resources :app_memipedia_users
   end
