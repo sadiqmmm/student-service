a8e1a2906d639f37845b32517c69b35917c1e0a0
jordanhudgens <jordan@devcamp.com>
2020-04-07 17:18:44 -0700

    Built out the custom serializers for the data manager system

--- app/controllers/memipedia/app_memipedia_posts_controller.rb
+++ app/controllers/memipedia/app_memipedia_posts_controller.rb
@@ -4,7 +4,7 @@ class Memipedia::MemipediaPostsController < MobileApplicationController
 
   def index
     if @client
-      render json: { app_memipedia_posts: @client.memipedia_posts.order(created_at: :desc) }
+      render json: { app_memipedia_posts: devcamp_space_serializer(@client) }
     else
       render json: { status: :unauthorized }
     end
@@ -16,7 +16,7 @@ class Memipedia::MemipediaPostsController < MobileApplicationController
       post.client = @client
 
       if post.save
-        render json: post
+        render json: { app_memipedia_user: serializer_post(post), status: :created }
       else
         render json: {
           status: 422,
@@ -24,15 +24,6 @@ class Memipedia::MemipediaPostsController < MobileApplicationController
           errors: post.errors
         }
       end
-    else
-      render json: { status: :unauthorized }
-    end
-  end
-
-  def show
-    if @client
-      post = MemipediaPost.find(params[:id])
-      render json: post
     else
       render json: { status: :unauthorized }
     end
@@ -65,20 +56,20 @@ class Memipedia::MemipediaPostsController < MobileApplicationController
           serializer_post post
         end
       else
-        user_object = Hash.new.as_json
-        user_object[:id] = 123
-        user_object[:email] = "user@example.com"
-        user_object[:password] = "ENCRYPTED"
-        user_object[:password_confirmation] = "ENCRYPTED"
-        user_object[:created_at] = Date.today
-        user_object[:column_names_merged_with_images] = [
+        post_object = Hash.new.as_json
+        post_object[:id] = 123
+        post_object[:name] = "Some cool meme"
+        post_object[:content] = "Content goes here"
+        post_object[:post_image] = "https://www.crondose.com/wp-content/uploads/2020/04/js-programming-meme.jpg"
+        post_object[:created_at] = Date.today
+        post_object[:column_names_merged_with_images] = [
           'id',
-          'email',
-          'password',
-          'password_confirmation',
+          'name',
+          'content',
+          'post_image',
           'created_at'
         ]
-        [user_object]
+        [post_object]
       end
     end
 
